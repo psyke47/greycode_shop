7846fdffc2ea35d729b91803fecbb7167c3cba6f
dakalomunonde <86750102+psyke47@users.noreply.github.com>
2026-01-29 09:24:03 +0200

    Add web app manifest and update favicon links in app layout
    - Created a new site.webmanifest file for the Greycode Shop with icons and theme settings.
    - Updated app.blade.php to include various favicon links and the web app manifest.
    - Adjusted the HTML language attribute to use the application's locale.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,9 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/png" href="/images/favicons/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/images/favicons/favicon.svg" />
+    <link rel="shortcut icon" href="/images/favicons/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="Greycode" />
+    <link rel="manifest" href="/images/favicons/site.webmanifest" />
     <title>Greycode Shop</title>
     @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/app.jsx'])
@@ -11,7 +17,7 @@
 </head>
 <body>
     @inertia
-    <div class="min-h-screen flex flex-col items-center justify-center bg-gray-500">
+    <div class="min-h-screen flex flex-col items-center justify-center bg-gray-900">
         <h1 class="text-9xl text-white">Greycode Shop</h1>
     </div>
 </body>
